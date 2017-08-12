04e7d3c1668d88211ded10e5e6675b36c30a95f5
Nicolas Rollier <nicolas@rollier.be>
2017-08-12 10:39:38 +0200

    Sanitize database dump

--- data/database.sql
+++ data/database.sql
@@ -1,12 +1,3 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 # Dump of table Data
 # ------------------------------------------------------------
 
@@ -21,7 +12,6 @@ CREATE TABLE `Data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `Data` WRITE;
-/*!40000 ALTER TABLE `Data` DISABLE KEYS */;
 
 INSERT INTO `Data` (`id`, `sensor_id`, `time`, `volume`)
 VALUES
@@ -29,9 +19,7 @@ VALUES
 	(21,'CA98D6D47381EC4E915555AF1F8F8',1434483135,2222),
 	(22,'A9B15494A7FC2EAB1C87353987868',1444524677,3333);
 
-/*!40000 ALTER TABLE `Data` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 # Dump of table Inventory
 # ------------------------------------------------------------
@@ -49,7 +37,6 @@ CREATE TABLE `Inventory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 LOCK TABLES `Inventory` WRITE;
-/*!40000 ALTER TABLE `Inventory` DISABLE KEYS */;
 
 INSERT INTO `Inventory` (`sensor_id`, `id_tap`, `id_beer`, `id_brewery`)
 VALUES
@@ -58,9 +45,7 @@ VALUES
 	('AA6B7FE9DD97B596D8B55168599FF',4,'Cuvee Delphine','\"De Struise Brouwers\"'),
 	('CA98D6D47381EC4E915555AF1F8F8',5,'Oude Kriek','\"3 Fonteinen\"');
 
-/*!40000 ALTER TABLE `Inventory` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 # Dump of table Sensors
 # ------------------------------------------------------------
@@ -78,7 +63,6 @@ CREATE TABLE `Sensors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 LOCK TABLES `Sensors` WRITE;
-/*!40000 ALTER TABLE `Sensors` DISABLE KEYS */;
 
 INSERT INTO `Sensors` (`client_id`, `client_name`, `client_address`, `sensor_id`, `tap_id`)
 VALUES
@@ -87,14 +71,4 @@ VALUES
 	(3,'De Struise Brouwers','Kasteelstraat 50, 8640 Oostvleteren','A9B15494A7FC2EAB1C87353987868',3),
 	(4,'De Struise Brouwers','Kasteelstraat 50, 8640 Oostvleteren','AA6B7FE9DD97B596D8B55168599FF',4);
 
-/*!40000 ALTER TABLE `Sensors` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
