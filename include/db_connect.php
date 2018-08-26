48161af1cdd25373c268cf518693e3057944fd4d
Nicolas Rollier <admin@iMac.local>
2018-08-26 15:10:35 +0200

    Changes to reflect README instructions

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,6 +1,6 @@
 <?php
  	## Replace host, user, password and database to reflect your environment
- 	$db = @mysqli_connect("host", "user", "password", "database");
+ 	$db = @mysqli_connect("hostname", "username", "password", "database");
 		if ($db->connect_errno) {
 			echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 		}
